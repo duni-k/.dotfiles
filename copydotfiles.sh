e84f6d0baaf853d1ec3fa06a39017e3000bd0462
Edvard All <edvard.all@gmail.com>
2024-01-21 09:59:25 +0100

    updated script and added polybar

--- copydotfiles.sh
+++ copydotfiles.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 cp ~/.config/i3/config ~/.dotfiles/i3/
 cp ~/.config/alacritty/alacritty.yml ~/.dotfiles/alacritty.yml
-cp -r ~/.config/lvim/ ~/.dotfiles/lvim
-cp -r ~/.config/helix/ ~/.dotfiles/helix
+cp -r ~/.config/polybar/  ~/.dotfiles/
+cp -r ~/.config/helix/ ~/.dotfiles/
 
 cp ~/.Xmodmap ~/.dotfiles
 cp ~/.Xresources ~/.dotfiles
