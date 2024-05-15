3314b1a16a25d317eda24242b6b4d289b5edae4a
dnk <edvard.all@gmail.com>
2024-05-15 09:59:23 +0200

    update

--- copydotfiles.sh
+++ copydotfiles.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/bash
-cp ~/.config/alacritty/alacritty.yml ~/.dotfiles/alacritty.yml
-cp -r ~/.config/polybar/  ~/.dotfiles/
+cp -r ~/.config/wezterm/wezterm.lua ~/.dotfiles/
 cp -r ~/.config/helix/ ~/.dotfiles/
 
-cp ~/.Xmodmap ~/.dotfiles
-cp ~/.Xresources ~/.dotfiles
-cp ~/.zshrc ~/.dotfiles
 echo "DONE"
